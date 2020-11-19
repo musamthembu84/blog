b705652d17bdfbfa5608e7006a354bd985e30e6c
musamthembu84@gmail.com <musamthembu84@gmail.com>
2020-11-19 22:55:34 +0200

    removing all unused imports

--- src/main/java/AI/Blog/service/PostService.java
+++ src/main/java/AI/Blog/service/PostService.java
@@ -1,13 +1,10 @@
 package AI.Blog.service;
 
-import AI.Blog.exception.ApplicationException;
 import AI.Blog.model.PostDao;
 import AI.Blog.repository.PostRepository;
-import AI.Blog.response.ErrorMessageResponse;
 import AI.Blog.response.SuccessMessageResponse;
 import com.google.common.base.Preconditions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
