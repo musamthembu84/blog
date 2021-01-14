d1af8cf313e1e09f338931cd13e30c88b38e5992
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-01-14 23:27:27 +0200

    adding repository for postdao will allow crud operations to be easier

--- src/main/java/AI/Blog/repository/PostRepository.java
+++ src/main/java/AI/Blog/repository/PostRepository.java
@@ -4,7 +4,6 @@ import AI.Blog.model.PostDao;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-//@Repository
-//extends CrudRepository<PostDao,Integer>
-public interface PostRepository {
+@Repository
+public interface PostRepository extends CrudRepository<PostDao,Integer>{
 }
