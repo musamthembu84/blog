66f3525e06b70836eb613754d5949ed39160db36
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-01-14 23:29:40 +0200

    creating postdao service validate pre conditions of content

--- src/main/java/AI/Blog/model/PostDao.java
+++ src/main/java/AI/Blog/model/PostDao.java
@@ -32,5 +32,7 @@ public class PostDao {
     @Column(columnDefinition = "postUpdatedDate")
     private Date postUpdatedDate;
 
-
+    public String getContent() {
+        return content;
+    }
 }
