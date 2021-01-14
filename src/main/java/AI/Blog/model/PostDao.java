c2954cebf60fe98c4bb37071a369bdfe01a916b5
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-01-14 23:26:18 +0200

    postdao created from scratch again and remove id from constructor

--- src/main/java/AI/Blog/model/PostDao.java
+++ src/main/java/AI/Blog/model/PostDao.java
@@ -1,76 +1,36 @@
 package AI.Blog.model;
-
 import javax.persistence.*;
 import java.util.Date;
 
-//@Entity
+@Entity
 public class PostDao {
 
-//    public PostDao() {
-//    }
-//
-//    public PostDao(final String content,final int views,
-//                   final Date creationDate,final Date postUpdatedDate) {
-//        this.id = id;
-//        this.content = content;
-//        this.views = views;
-//        this.postCreationDate = creationDate;
-//        this.postUpdatedDate = postUpdatedDate;
-//    }
+    public PostDao() {
+    }
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private int id;
-//
-//    @Column(columnDefinition = "content")
-//    private String content;
-//
-//    @Column(columnDefinition = "views")
-//    private int views;
-//
-//    @Column(columnDefinition = "postCreationDate")
-//    private Date postCreationDate;
-//
-//    @Column(columnDefinition = "postUpdatedDate")
-//    private Date postUpdatedDate;
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getContent() {
-//        return content;
-//    }
-//
-//    public void setContent(String content) {
-//        this.content = content;
-//    }
-//
-//    public int getViews() {
-//        return views;
-//    }
-//
-//    public void setViews(int views) {
-//        this.views = views;
-//    }
-//
-//    public Date getPostCreationDate() {
-//        return postCreationDate;
-//    }
-//
-//    public void setPostCreationDate(Date postCreationDate) {
-//        this.postCreationDate = postCreationDate;
-//    }
-//
-//    public Date getPostUpdatedDate() {
-//        return postUpdatedDate;
-//    }
-//
-//    public void setPostUpdatedDate(Date updatedDate) {
-//        this.postUpdatedDate = updatedDate;
-//    }
+    public PostDao(final String content,final int views,
+                   final Date creationDate,final Date postUpdatedDate) {
+        this.content = content;
+        this.views = views;
+        this.postCreationDate = creationDate;
+        this.postUpdatedDate = postUpdatedDate;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(columnDefinition = "content")
+    private String content;
+
+    @Column(columnDefinition = "views")
+    private int views;
+
+    @Column(columnDefinition = "postCreationDate")
+    private Date postCreationDate;
+
+    @Column(columnDefinition = "postUpdatedDate")
+    private Date postUpdatedDate;
+
+
 }
