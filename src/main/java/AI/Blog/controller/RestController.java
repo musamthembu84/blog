f0e329cb743cb095c7727e7661f8a6cc45f8a53f
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-01-19 22:04:09 +0200

    extends application exception to RestController

--- src/main/java/AI/Blog/controller/RestController.java
+++ src/main/java/AI/Blog/controller/RestController.java
@@ -1,14 +1,11 @@
 package AI.Blog.controller;
+import AI.Blog.exception.ApplicationException;
 import AI.Blog.model.PostDao;
 import AI.Blog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.Date;
-
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
@@ -20,14 +17,8 @@ public class RestController {
     }
 
     @PostMapping(path = "/posting")
-    public ResponseEntity<Object> createPost(final @RequestBody PostDao postDao) {
+    public ResponseEntity<Object> createPost(final @RequestBody PostDao postDao) throws ApplicationException {
         return postService.createPost(postDao);
     }
 
-    @GetMapping(path = "/likes")
-    public String likes(){
-        PostDao postDao  = new PostDao("sd",0, new java.util.Date(),new java.util.Date());
-        postService.createPost(postDao);
-        return "haha";
-    }
 }
