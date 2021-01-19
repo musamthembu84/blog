6bbd3adf46e9b156dc7063bcff38e5dde7477963
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-01-19 23:14:04 +0200

    implement service for all likes

--- src/main/java/AI/Blog/service/LikeService.java
+++ src/main/java/AI/Blog/service/LikeService.java
@@ -7,23 +7,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-public class LikeService{
+public class LikeService implements ILikes{
 
-//    private final LikesRepository likes;
-//
-//    @Autowired
-//    public LikeService(LikesRepository likes) {
-//        this.likes = likes;
-//    }
-//
-//    @Override
-//    public ResponseEntity<Object> incrementLike(final int likeID) {
-//
-//        int currentLikeCounter = likes.getLikesForPost(likeID);
-//
-//        LikesDao likesDao = new LikesDao();
-//
-//        //increment counter where id = postid
-//        return null;
-//    }
+    private LikesRepository repository;
+
+    @Autowired
+    public LikeService(LikesRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public ResponseEntity<Object> incrementLike(int likeID) {
+
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> viewTotalLikes(int likeID) {
+        return null;
+    }
 }
