0428083b2787a0c2803c0a4f2a612244dfea000a
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-01-19 23:11:02 +0200

    defining model and constructor of Likes

--- src/main/java/AI/Blog/model/LikesDao.java
+++ src/main/java/AI/Blog/model/LikesDao.java
@@ -5,6 +5,15 @@ import javax.persistence.*;
 @Entity
 public class LikesDao {
 
+    public LikesDao() {
+    }
+
+    public LikesDao(final Integer id,final int counter,final PostDao postDao) {
+        this.id = id;
+        this.counter = counter;
+        this.postDao = postDao;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
