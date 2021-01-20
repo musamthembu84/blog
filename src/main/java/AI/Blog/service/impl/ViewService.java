31a00428fc954449d9d6863ca0dcece9924e0eb0
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-01-20 23:35:10 +0200

    creating a method to do the actual setting of view

--- src/main/java/AI/Blog/service/impl/ViewService.java
+++ src/main/java/AI/Blog/service/impl/ViewService.java
@@ -1,12 +1,15 @@
 package AI.Blog.service.impl;
 
+import AI.Blog.model.PostDao;
 import AI.Blog.repository.LikesRepository;
 import AI.Blog.repository.ViewRepository;
 import AI.Blog.response.LikesResponseMessage;
 import AI.Blog.service.interaces.IViews;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ViewService implements IViews {
 
     private final ViewRepository viewRepository;
@@ -20,4 +23,19 @@ public class ViewService implements IViews {
     public ResponseEntity<Object> getViewForPost(final int postID) {
         return ResponseEntity.ok().body(LikesResponseMessage.createMessage(viewRepository.getTotalViewsForPost(postID) + "Likes"));
     }
+
+    @Override
+    public ResponseEntity<Object> addViewForPost(final int postID) {
+        createNewPostForView(postID);
+
+        return ResponseEntity.ok().body(LikesResponseMessage.createMessage("View Added For Post" + postID));
+    }
+
+    private PostDao createNewPostForView(final int postId){
+        PostDao postDao = new PostDao();
+        postDao.setPost_id(postId);
+        return postDao;
+    }
+
+
 }
--- src/main/java/AI/Blog/service/interaces/IViews.java
+++ src/main/java/AI/Blog/service/interaces/IViews.java
@@ -1,8 +1,10 @@
 package AI.Blog.service.interaces;
 
+import AI.Blog.model.PostDao;
 import org.springframework.http.ResponseEntity;
 
 public interface IViews {
 
     ResponseEntity<Object> getViewForPost(final int postId);
+    ResponseEntity<Object> addViewForPost(final int postID);
 }
