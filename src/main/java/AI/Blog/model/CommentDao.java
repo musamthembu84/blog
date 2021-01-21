15e06e02b4731f3d3b44620202d0aa1087bb3095
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-01-21 21:36:18 +0200

    creating CommentDao model

--- src/main/java/AI/Blog/model/CommentDao.java
+++ src/main/java/AI/Blog/model/CommentDao.java
@@ -1,6 +1,7 @@
 package AI.Blog.model;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class CommentDao {
@@ -8,15 +9,26 @@ public class CommentDao {
     public CommentDao() {
     }
 
-    public CommentDao(Integer id, String commentContent) {
-        this.id = id;
-        this.commentContent = commentContent;
+    public CommentDao(String content, String author, Date commentDate) {
+        this.content = content;
+        this.author = author;
+        this.commentDate = commentDate;
     }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private int comment_id;
 
     @Column(nullable = false)
-    private String commentContent;
+    private String content;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private Date commentDate;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id",nullable = false)
+    private PostDao postDao;
 }
