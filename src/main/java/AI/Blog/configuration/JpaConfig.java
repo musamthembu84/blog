cf34706c0f888ef47e3e311434f46656596f49f6
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-01-27 20:43:09 +0200

    adding swagger dependency on gradle build configure class to enable swagger use

--- src/main/java/AI/Blog/configuration/JpaConfig.java
+++ src/main/java/AI/Blog/configuration/JpaConfig.java
@@ -9,11 +9,11 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import javax.sql.DataSource;
-import java.util.Objects;
 
 @Configuration
+@EnableSwagger2
 public class JpaConfig {
 
     private final Environment env;
@@ -33,16 +33,14 @@ public class JpaConfig {
 
         return dataSource;
     }
-//
-//    @Bean
-//    public Docket api(){
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
 
-
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 
 }
