abd991ec4a5a3dd35c73c36f3d455f49c60842b5
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-02-09 21:23:29 +0200

    bypass cross origin error on frontend

--- src/main/java/AI/Blog/controller/RestController.java
+++ src/main/java/AI/Blog/controller/RestController.java
@@ -8,10 +8,7 @@ import AI.Blog.service.impl.PostService;
 import AI.Blog.service.impl.ViewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,6 +36,7 @@ public class RestController {
         return postService.createPost(postDao);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(path = "/getPosts")
     public List<PostDao> retrievalPosts(@RequestParam int numberOfPosts){
         return postService.retrievePosts(numberOfPosts);
