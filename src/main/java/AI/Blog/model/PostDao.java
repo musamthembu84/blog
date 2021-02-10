79b8510c538453bad1661e83751bb16f99eb2cc3
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-02-10 21:54:38 +0200

    adding a field called as sub_content for post_dao

--- src/main/java/AI/Blog/model/PostDao.java
+++ src/main/java/AI/Blog/model/PostDao.java
@@ -9,12 +9,15 @@ public class PostDao {
     public PostDao() {
     }
 
-    public PostDao(final int post_id,final String content,final String title, final Date post_creation_date, final Date post_updated_date) {
+    public PostDao(final int post_id,final String content,final String title,
+                   final Date post_creation_date, final Date post_updated_date,
+                   final String sub_content) {
         this.post_id = post_id;
         this.content = content;
         this.title = title;
         this.post_creation_date = post_creation_date;
         this.post_updated_date = post_updated_date;
+        this.sub_content = sub_content;
     }
 
     @Id
@@ -23,6 +26,9 @@ public class PostDao {
 
     @Column(nullable = false)
     private String content;
+
+    @Column(nullable = false)
+    private String sub_content;
 
     @Column(nullable = false)
     private String title;
@@ -66,5 +72,13 @@ public class PostDao {
 
     public Date getPost_updated_date() {
         return post_updated_date;
+    }
+
+    public String getSub_content() {
+        return sub_content;
+    }
+
+    public void setSub_content(String sub_content) {
+        this.sub_content = sub_content;
     }
 }
